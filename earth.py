a7a089066ab89cbb596bf9538d41c52a214cc8cc
Lludion <ulysse.remond@outlook.fr>
2020-09-13 12:06:28 +0200

    Added connections in earth.py
    - the board's connection are copied (I only added a transitions from madagascar to western australia)
    - scandinavia renamed

--- earth.py
+++ earth.py
@@ -1,7 +1,4 @@
 from riskwizou import *
-from abstract import *
-
-game = Game()
 
 ## Continents
 
@@ -27,7 +24,7 @@ groenland = Zone("groenland",na)
 na.adds([alaska,nunavut,alberta,ontario,quebec,westernstates,easternstates,centralamerica,groenland])
 
 #Europe
-europe.adds(["iceland",'greatbritain',"scandinavie","ukraine","north","south","west"])
+europe.adds(["iceland",'greatbritain',"scandinavia","ukraine","north","south","west"])
 
 #Asia
 asia.adds(["afghanistan","ural","siberia","tchita","mongolia","yakutia","china","japan","kamschatka","middleeastern","india","siam"])
@@ -44,4 +41,49 @@ sa.adds(["brasil","peru","venezuela","argentina"])
 #earth
 earth = World([na,sa,asia,europe,africa,oceania])
 
+## Connections
 
+#North America
+
+earth["alaska"].add_conns(earth["kamschatka"],earth["nunavut"],earth["alberta"])
+earth["alberta"].add_conns(earth["nunavut"],earth["ontario"],earth["westernstates"])
+earth["ontario"].add_conns(earth["nunavut"],earth["groenland"],earth["quebec"],earth["easternstates"],earth["westernstates"])
+earth["groenland"].add_conns(earth["nunavut"],earth["quebec"],earth["iceland"])
+earth["easternstates"].add_conns(earth["quebec"],earth["westernstates"],earth["centralamerica"])
+earth["centralamerica"].add_conns(earth["easternstates"],earth["westernstates"],earth["venezuela"])
+
+#Europe
+
+earth["iceland"].add_conns(earth["scandinavia"],earth["greatbritain"])
+earth["greatbritain"].add_conns(earth["scandinavia"],earth["west"],earth["north"])
+earth["scandinavia"].add_conns(earth["north"],earth["ukraine"])
+earth["north"].add_conns(earth["south"],earth["west"],earth["ukraine"])
+earth["south"].add_conns(earth["west"],earth["ukraine"])
+
+#Asia
+
+earth["ural"].add_conns(earth["ukraine"],earth["afghanistan"],"china","siberia")
+earth["china"].add_conns("mongolia","afghanistan","siberia","india","siam")
+earth["tchita"].add_conns("mongolia","siberia","yakutia","kamschatka")
+earth["japan"].add_conns("mongolia","kamschatka")
+earth["middleeastern"].add_conns("ukraine","egypt","afghanistan","india","eastafrica")
+earth["siam"].add_conns("india","indonesia")
+
+#Oceania
+earth["westaustralia"].add_conns("newguinea","eastaustralia","madagascar","indonesia")
+earth["newguinea"].add_conns("indonesia","eastaustralia")
+
+#Africa
+earth["madagascar"].add_conns("southafrica","eastafrica")
+earth["congo"].add_conns("southafrica","eastafrica","northafrica")
+earth["egypt"].add_conns("eastafrica","northafrica","south")
+earth["northafrica"].add_conns("west","brasil","eastafrica")
+earth["southafrica"].add_conns("eastafrica")
+
+#South America
+earth["brasil"].add_conns("venezuela","argentina","peru")
+earth["peru"].add_conns("venezuela","argentina")
+
+#global
+earth.purify_transitions()
+earth.activate_transitions()
