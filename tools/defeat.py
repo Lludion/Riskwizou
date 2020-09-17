2a9e139c7cb9a6cd61c759bb22c63999fbe8ec9a
Lludion <ulysse.remond@outlook.fr>
2020-09-17 19:55:41 +0200

    Removed bug : Attackers were never killed
    - as the pm is removed after the fight, defeat did not work properly

--- tools/defeat.py
+++ tools/defeat.py
@@ -2,7 +2,7 @@
 def defeat(units,survivors=""):
     # units is defeated, some units die
     for u in units:
-        if u.pm >= 1 and survivors != "no":
+        if u.pm >= 2 and survivors != "no":
             u.pm -= 1
         else:
             u.dies()
